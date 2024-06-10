31f4f8685d41cd9b3ee21d2ab92f28932f873b1f
rezasaft <rezasaft13@gmail.com>
2024-06-10 22:47:42 +0700

    rezasaft

--- themes/dashboard/views/admin/add_questions.blade.php
+++ themes/dashboard/views/admin/add_questions.blade.php
@@ -10,12 +10,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Add questions</h1>
+            <h1 class="m-0">Tambah Pertanyaan</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Add questions</li>
+              <li class="breadcrumb-item active">Tambah Pertanyaan</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
@@ -31,7 +31,7 @@
                   <h3 class="card-title">Title</h3>
   
                   <div class="card-tools">
-                        <a class="btn btn-info btn-sm" href="javascript:;" data-toggle="modal" data-target="#myModal">Add new</a>
+                        <a class="btn btn-info btn-sm" href="javascript:;" data-toggle="modal" data-target="#myModal">tambah</a>
                   </div>
                 </div>
                 <div class="card-body">
@@ -39,10 +39,10 @@
                         <thead>
                             <tr>
                                 <th>#</th>
-                                <th>Question</th>
-                                <th>ans</th>
+                                <th>Pertanyaan</th>
+                                <th>Jawaban</th>
                                 <th>Status</th>
-                                <th>Actions</th>
+                                <th>Aksi</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -62,10 +62,10 @@
                         <tfoot>
                             <tr>
                                 <th>#</th>
-                                <th>Question</th>
-                                <th>ans</th>
+                                <th>Pertanyaan</th>
+                                <th>Jawaban</th>
                                 <th>Status</th>
-                                <th>Actions</th>
+                                <th>Aksi</th>
                             </tr>
                         </tfoot>
                     </table>
@@ -87,7 +87,7 @@
       <!-- Modal content-->
       <div class="modal-content">
         <div class="modal-header">
-          <h4 class="modal-title">Add new Question</h4>
+          <h4 class="modal-title">Tambah Pertanyaan Baru</h4>
           <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
         <div class="modal-body">
@@ -95,7 +95,7 @@
                 <div class="row">
                     <div class="col-sm-12">
                         <div class="form-group">
-                            <label for="">Enter Question</label>
+                            <label for="">Masukan Pertanyaan</label>
                             {{ csrf_field()}}
                             <input type="hidden" name="exam_id" value="{{ Request::segment(3)}}">
                             <input type="text" required="required" name="question" placeholder="Enter Question" class="form-control">
@@ -103,50 +103,50 @@
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
-                            <label for="">Enter Option 1</label>
+                            <label for="">Pilihan 1</label>
                             <input type="text" required="required" name="option_1" placeholder="Enter Question" class="form-control">
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
-                            <label for="">Enter Option 2</label>
+                            <label for="">Pilihan 2</label>
                             <input type="text" required="required" name="option_2" placeholder="Enter Option 2" class="form-control">
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
-                            <label for="">Enter Option 3</label>
+                            <label for="">Pilihan 3</label>
                             <input type="text" required="required" name="option_3" placeholder="Enter  Option 3" class="form-control">
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
-                            <label for="">Enter Option 4</label>
+                            <label for="">Pilihan 4</label>
                             <input type="text" required="required" name="option_4" placeholder="Enter  Option 4" class="form-control">
                         </div>
                     </div>
                     {{-- <div class="col-sm-12">
                         <div class="form-group">
-                            <label for="">Enter correct ans</label>
+                            <label for="">Jawaban Benar</label>
                             <input type="text" required="required" name="ans" placeholder="Enter  correct ans" class="form-control">
                         </div>
                     </div> --}}
                     <div class="form-group">
-                      <label for="">Select correct option</label>
+                      <label for="">Jawaban Benar</label>
                       <select class="form-control" required="required" name="ans">
                           <option value="">Select</option>
                         
-                          <option value="option_1">option 1</option>
-                          <option value="option_2">option 2</option>
-                          <option value="option_3">option 3</option>
-                          <option value="option_4">option 4</option>
+                          <option value="option_1">Pilihan 1</option>
+                          <option value="option_2">Pilihan 2</option>
+                          <option value="option_3">Pilihan 3</option>
+                          <option value="option_4">Pilihan 4</option>
           
                       </select>
                   </div>
 
                     <div class="col-sm-12">
                         <div class="form-group">
-                            <button class="btn btn-primary">Add</button>
+                            <button class="btn btn-primary">Tambahkan</button>
                         </div>
                     </div>
                 </div>
--- themes/dashboard/views/admin/edit_category.blade.php
+++ themes/dashboard/views/admin/edit_category.blade.php
@@ -10,12 +10,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Edit Category</h1>
+            <h1 class="m-0">Edit Kategori</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Edit Category</li>
+              <li class="breadcrumb-item active">Edit Kategori</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
@@ -33,7 +33,7 @@
                         <div class="row">
                             <div class="col-sm-12">
                                 <div class="form-group">
-                                    <label for="">Enter category name</label>
+                                    <label for="">Masukkan Kategori</label>
                                     {{ csrf_field()}}
                                     <input type="hidden" name="id" value="{{ $category->id}}">
                                     <input type="text" required="required" value="{{ $category->name}}" name="name" placeholder="Enter category name" class="form-control">
@@ -64,7 +64,7 @@
       <!-- Modal content-->
       <div class="modal-content">
         <div class="modal-header">
-          <h4 class="modal-title">Add new category</h4>
+          <h4 class="modal-title">Tambahkan Kategori Baru</h4>
           <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
         <div class="modal-body">
